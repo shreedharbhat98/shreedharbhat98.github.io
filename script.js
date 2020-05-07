8b11bd231faa78a03a06777f6c7859b909bfcd69
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-05-07 16:34:53 +0530

    First Card

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.getElementById("linkedIn").addEventListener("click", function () {
+    window.location.href = "https://www.linkedin.com/in/shreedhar-bhat-34bb89139/"
+})
+
+document.getElementById("github").addEventListener("click", function () {
+    window.location.href = "https://github.com/shreedharbhat98"
+})
+
+document.getElementById("twitter").addEventListener("click", function () {
+    window.location.href = "https://twitter.com/Shreedh29725938"
+})
+
+document.getElementById("hackerrank").addEventListener("click", function () {
+    window.location.href = "https://www.hackerrank.com/Shridharbhat1998"
+})
